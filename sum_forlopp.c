e50cf840eabfee5a17ad3045a286aa0617c4b324
Rokibul Islam <rokib.hridoy@gmail.com>
2021-03-27 12:51:26 +0600

    .

--- sum_forlopp.c
+++ sum_forlopp.c
@@ -1,25 +1,27 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int sum(int arr[], int n) {
+int sum(int arr[], int n)
+{
     int s = 0;
     int i;
-    for (i=0;i<n;i++){
+    for (i = 0; i < n; i++)
+    {
         s += arr[i];
     }
     return s;
 }
 
-
-int main() {
+int main()
+{
     int arr[100];
-    int i,j,k,n;
+    int i, j, k, n;
 
     printf("Enter size of array ");
-    scanf("%d",&n);
-    for(i=0;i<n;i++)
+    scanf("%d", &n);
+    for (i = 0; i < n; i++)
     {
-        printf("%dth element ",i+1);
-        scanf("%d",&arr[i]);
+        printf("%dth element ", i + 1);
+        scanf("%d", &arr[i]);
     }
 
     printf("sum of the elements of this array is %d", sum(arr, n));
